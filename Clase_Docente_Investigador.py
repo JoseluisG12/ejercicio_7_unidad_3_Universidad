cf92426bb35ee3678b8ab4f22adb05126965982c
JoseluisG <119555086+JoseluisG12@users.noreply.github.com>
2023-06-07 22:10:04 -0300

    Initial commit

---
+++ Clase_Docente_Investigador.py
@@ -0,0 +1,35 @@
+from Clase_Docente import Docente
+from Clase_Inversitagor import  Investigador
+
+class Docente_Invertigador(Docente,Investigador):
+    __categoria_inv : int
+    __importe : float
+
+    def __init__(self, cuil, apellido, nombre, sueldoBasico, antiguedad,carrera='', cargo='', catedra='', areaInvest='', tipoInvest='',categoria_inv='',importe=0) -> None:
+        super().__init__(cuil, apellido, nombre, sueldoBasico, antiguedad,carrera,cargo, catedra, areaInvest,tipoInvest)
+        self.__categoria_inv = int(categoria_inv)
+        self.__importe = importe
+
+
+
+
+
+
+    def getimporte(self):
+        return self.__importe
+
+    def getsueldo(self):
+        sueldo = super().getsueldoB()
+        return sueldo
+
+    def getcategoria(self):
+        return self.__categoria_inv
+
+
+    def mostrardatos(self):
+        super().mostrardatos()
+        print("""
+Datos docente y ayudante:""")
+        print(f"""
+Categoria:{self.__categoria_inv}
+Importe:${self.__importe}""")
